8d725fab04e17979b838f4ac094fdd3168a15be1
sameer arshad <sameerarshad7771@gmail.com>
2022-07-01 23:26:41 +0500

    added 'DEFAULT_PAGINATION_CLASS' on RF settings

--- core/settings.py
+++ core/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 AUTH_USER_MODEL = 'authentication.User'
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
     'NON_FIELD_ERRORS_KEY': 'error',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
@@ -67,6 +69,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'drf_yasg',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
