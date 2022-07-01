8653399557a932225ed7e86911234333f09309e5
sameer arshad <sameerarshad7771@gmail.com>
2022-07-01 21:05:35 +0500

    change urls pattern

--- core/urls.py
+++ core/urls.py
@@ -41,6 +41,6 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
     path('admin/', admin.site.urls),
-    path('', include('authentication.urls')),
+    path('auth/', include('authentication.urls')),
     path('', include('expenses.urls')),
 ]
