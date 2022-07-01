366ff47e71e969592674bdbe8128ab393ab94a5e
sameer arshad <sameerarshad7771@gmail.com>
2022-07-01 23:35:48 +0500

    Added django_filters on income app

--- income/views.py
+++ income/views.py
@@ -1,15 +1,18 @@
-from rest_framework import permissions
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-
 from .serializers import IncomeSerializer
 from .models import Income
 from .permissions import IsOwner
+
+from rest_framework import permissions
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class IncomeCreateAPIView(ListCreateAPIView):
     serializer_class = IncomeSerializer
     permission_classes = (permissions.IsAuthenticated,)
     queryset = Income.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ('id', 'owner', 'amount', 'source',)
 
     def perform_create(self, serializer):
         return serializer.save(owner=self.request.user)
