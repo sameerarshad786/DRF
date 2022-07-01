8a0dcdae09912e903caf592ced29572fe9b3c82a
sameer arshad <sameerarshad7771@gmail.com>
2022-07-01 23:25:24 +0500

    Remove permission classes from ExpenseCreateAPICiew

--- expenses/views.py
+++ expenses/views.py
@@ -1,21 +1,20 @@
-from rest_framework import permissions
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-
 from .serializers import ExpenseSerializer
 from .models import Expense
 from .permissions import IsOwner
 
+from rest_framework import permissions
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+
+from django_filters.rest_framework import DjangoFilterBackend
 
 class ExpenseCreateAPIView(ListCreateAPIView):
     serializer_class = ExpenseSerializer
-    permission_classes = (permissions.IsAuthenticated,)
     queryset = Expense.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ('id', 'owner', 'amount', 'category',)
 
     def perform_create(self, serializer):
         return serializer.save(owner=self.request.user)
-
-    def get_queryset(self):
-        return self.queryset.filter(owner=self.request.user)
 
 class ExpenseRetrieveAPIView(RetrieveUpdateDestroyAPIView):
     serializer_class = ExpenseSerializer
